d63fe2627a527d3143442d91617238b43457caea
Echaarani Mohamed <echaaranimohamed@gmail.com>
2019-04-19 09:47:25 +0100

    navigation bar fixed

--- js/main.js
+++ js/main.js
@@ -1,11 +1,81 @@
 let burger = document.getElementById('burger-button');
 let menu_BG = document.getElementById('menu-BG');
 let menu_nav = document.getElementById('menu-nav');
-
+let body = document.getElementsByTagName('body')[0];
 
 burger.addEventListener('click',()=>{
     menu_BG.classList.toggle('burger_Clicked');
     burger.classList.toggle('is-active')
     menu_nav.classList.toggle('ShowMenu');
     setTimeout(()=>menu_nav.classList.toggle('oppacityMenuNav'),100);
+    body.classList.toggle('positionFixed');
 })
+
+
+/* Comment Ca march Navigation tabs */
+
+let comCaMarchContent = [
+{
+    Number:'01',
+    image:'./img/samsung1.png',
+    svgIcon:'./img/note.svg',
+    title:'INSCRIVEZ-VOUS',
+    content:"Créez directement votre compte Foodeals, ou via la synchronisation avec vos comptes Facebook ou Gmail."},
+{
+    Number:'02',
+    image:'./img/samsung2.png',
+    svgIcon:'./img/location.svg',
+    title:'LOCALISEZ-VOUS',
+    content:'Cherchez des restaurants, boulangeries, cafés, ou même des supermarchés à proximité qui offrent des plats ou produits qui répondront à vos besoins, tout en rationalisant vos achats quotidiens.'},
+{
+    Number:'03',
+    image:'./img/samsung3.png',
+    svgIcon:'./img/product.svg',
+    title:'PASSEZ VOTRE COMMANDE',
+    content:'Choisissez votre repas à n’importe quel moment, confirmer l’heure de la récupération, payez facilement et en toute sécurité via l’application et enregistrez le code obtenu.'},
+{
+    Number:'04',
+    image:'./img/samsung4.png',
+    svgIcon:'./img/tray.svg',
+    title:'RÉCUPÉREZ VOTRE PANIER',
+    content:'A l’heure prévue passez au point de vente, présentez votre code et retrouvez votre achat.'}
+];
+
+let radioTabs = document.getElementsByName('navTabs');
+
+let commeCaImg = document.getElementById('commeCaImg');
+let commeCaNumber = document.getElementById('commeCaNumber');
+let commeCaSVG = document.getElementById('commeCaSVG');
+let commeCaTitle = document.getElementById('commeCaTitle');
+let commeCaContent = document.getElementById('commeCaContent');
+let comCaMarchPartRight = document.getElementById('comCaMarchPartRight');
+
+
+function navTabFunc(event){
+    if(event.target.tagName == "INPUT"){
+        for( let i = 0 ; i < radioTabs.length ; i++ ){
+            if(radioTabs[i].checked){
+                comCaMarchPartRight.style.opacity = 0;
+                setTimeout(()=>{
+                    commeCaImg.src = comCaMarchContent[i].image;
+                    commeCaNumber.innerHTML = comCaMarchContent[i].Number;
+                    commeCaSVG.src = comCaMarchContent[i].svgIcon;
+                    commeCaTitle.innerHTML = comCaMarchContent[i].title;
+                    commeCaContent.innerHTML = comCaMarchContent[i].content;
+                    comCaMarchPartRight.style.opacity = 1;
+                } ,300); 
+            }
+        }
+    }
+}
+
+/* scroll fix navbar */ 
+
+function scrollFixNav(){
+    console.log(window.innerWidth);
+    if(window.scrollY >= 80){
+        // document.getElementsByTagName('nav')[0].classList.add('positionFixed');
+    }
+    // console.log(document.body.scrollTop)
+    
+}   
