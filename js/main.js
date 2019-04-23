b916fa9fa8713beb3aa55f14c3739ee15e762e49
Echaarani Mohamed <echaaranimohamed@gmail.com>
2019-04-23 11:15:45 +0100

    slider responsive added

--- js/main.js
+++ js/main.js
@@ -107,13 +107,40 @@ function scrollFixNav(){
         document.getElementsByClassName('hamburger-inner')[0].classList.remove('changeBurgerColor');
         document.getElementById('partnerButton').classList.remove('partnerMobileScroll');
     }
-    console.log(window.scrollY);
 } 
+
+let testimonyContainer = document.getElementsByClassName('testimony-container')[0];
 
 function removeClasses(){
     if(window.innerWidth<768){
         navContainer.classList.remove('navFixedScrollUp');
         navContainer.classList.remove('navScrollDown');
         partnerButton.classList.remove('partnerStyleOnSCroll');
-    }  
+        // testimonyContainer.classList.add('center');
+    }
 }
+
+/* Testimoials Script */
+
+let profilePics = document.getElementsByClassName('testimony-pic');
+let socialMediaDivs = document.getElementsByClassName('testimony-social-media');
+
+function show(event){
+    let numberOfDiv = parseInt(event.target.id[event.target.id.length - 1]);
+    socialMediaDivs[numberOfDiv - 1].style.display = 'flex ';
+    setTimeout( () => socialMediaDivs[numberOfDiv - 1].classList.add('show'),100);
+}
+
+function hideOnLeave(event){
+    for(let i = 0 ; i < socialMediaDivs.length ; i++){
+        socialMediaDivs[i].classList.remove('show');
+        setTimeout( () => socialMediaDivs[i].style.display = 'none' ,100);
+    }
+}
+
+
+/* Draggable slide Script */
+
+
+
+
